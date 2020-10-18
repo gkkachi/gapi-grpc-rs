1187ad1032416dc2b954f667111c02a3407ce581
gkkachi <18605580+gkkachi@users.noreply.github.com>
2020-10-18 00:41:04 +0000

    Deploying to gh-pages from  @ c899701546933bbb0eda56bcfb90e200b8d0af06 🚀

--- gapi_grpc/google/cloud/bigquery/v2/model/training_run/iteration_result/sidebar-items.js
+++ gapi_grpc/google/cloud/bigquery/v2/model/training_run/iteration_result/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ClusterInfo","Information about a single cluster for clustering model."]]});
+initSidebarItems({"mod":[["arima_result",""]],"struct":[["ArimaResult","(Auto-)arima fitting result. Wrap everything in ArimaResult for easier refactoring if we want to use model-specific iteration results."],["ClusterInfo","Information about a single cluster for clustering model."]]});
--- implementors/http_body/trait.Body.js
+++ implementors/http_body/trait.Body.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
 implementors["http_body"] = [];
-implementors["tonic"] = [{"text":"impl <a class=\"trait\" href=\"http_body/trait.Body.html\" title=\"trait http_body::Body\">Body</a> for <a class=\"struct\" href=\"tonic/body/struct.BoxBody.html\" title=\"struct tonic::body::BoxBody\">BoxBody</a>","synthetic":false,"types":["tonic::body::BoxBody"]}];
+implementors["tonic"] = [{"text":"impl Body for BoxBody","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
